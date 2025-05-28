667ba3e3ceac2c961e1df89b78ef906ae87b663e
Alessandro Jablonski <34753426+ale2502@users.noreply.github.com>
2025-05-27 22:23:37 -0300

    Still thinking...

--- 14/test.js
+++ 14/test.js
@@ -1,8 +1,23 @@
-const strs = ["a", "a"];
-const arrayOfArray = strs.map(str => str.split(""));
-
-for (let i = 0; i < arrayOfArray.length; i++) {
-    if (arrayOfArray[0][0] === arrayOfArray [i][0]) {
-        
+const strings = ["flower","flow","flight"];
+const arrayLength = strings.length;
+const shortestString = Math.min(...strings.map(x => x.length));
+let j = 0;
+// arrayLength = 3
+// shortesString = 4
+if (arrayLength > 1) {
+    for (let j = 0; j = arrayLength; j++) {
+        for (let i = 1; i < arrayLength; i++) {
+            if (strings[0][j] === strings[i][j]) {
+                first = string[0][0];
+            } else {
+                return "";
+            }
+        }
     }
+} else {
+    return string[0];
 }
+
+
+
+console.log(arrayLength);
