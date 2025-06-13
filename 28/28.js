09bb6ad9e9bfee3be7a1861392d5f023fb10ec46
Alessandro Jablonski <34753426+ale2502@users.noreply.github.com>
2025-06-13 14:54:28 -0300

    Problem 28

---
+++ 28/28.js
@@ -0,0 +1,10 @@
+const haystack = '';
+const needle = '';
+
+for (let i = 0; i < haystack.length; i++) {
+    for (let k = 0; k < needle.length; k++) {
+        if (haystack[i] === needle[k]) {
+            
+        }
+    }
+}
